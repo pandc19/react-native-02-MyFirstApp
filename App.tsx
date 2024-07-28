7fe63a2f5ef09e70b77e4a97e316f28b461cd454
Jonathan Pillajo <pandc19@outlook.com>
2024-07-28 16:54:06 -0500

    end section 5

--- App.tsx
+++ App.tsx
@@ -5,6 +5,12 @@ import { PaperProvider } from 'react-native-paper';
 import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 
 import IonIcon from 'react-native-vector-icons/Ionicons';
+import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen';
+import { DimensionScreen } from './src/presentation/screens/DimensionScreen';
+import { PositionScreen } from './src/presentation/screens/PositionScreen';
+import { FlexScreen } from './src/presentation/screens/FlexScreen';
+import { FlexDirectionScreen } from './src/presentation/screens/FlexDirectionScreen';
+import { HomeworkScreen } from './src/presentation/screens/HomeworkScreen';
 
 export const App = () => {
   return (
@@ -16,7 +22,13 @@ export const App = () => {
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloWorldScreen name='Jonathan Pillajo'/> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        {/* <BoxObjectModelScreen /> */}
+        {/* <DimensionScreen /> */}
+        {/* <PositionScreen /> */}
+        {/* <FlexScreen /> */}
+        {/* <FlexDirectionScreen /> */}
+        <HomeworkScreen />
       </SafeAreaView>
     </PaperProvider>
   );
